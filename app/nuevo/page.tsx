1e9abf3c28a78628f30c97f8833753d0a341c6bf
Luciano Fontanarrosa <lucianofonta9@gmail.com>
2026-05-26 19:58:08 -0300

    Corrección suspensive

--- app/nuevo/page.tsx
+++ app/nuevo/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import NuevoTicket from "../componentes/NuevoTicket";
 
 export default function Page() {
-  return <NuevoTicket />;
+  return (
+    <Suspense fallback={<div className="p-4 text-center text-gray-500">Cargando formulario...</div>}>
+      <NuevoTicket />
+    </Suspense>
+  );
 }
